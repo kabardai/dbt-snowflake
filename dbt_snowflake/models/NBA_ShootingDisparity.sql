5abcd8d07b8ed03d9e387219a4e76187fee1fbd6
Kabir Bardai <kabardai230@gmail.com>
2025-10-14 12:06:28 -0700

    introduced 23-24 shooting data, started doing basic 3P% analysis between years

--- dbt_snowflake/models/NBA_ShootingDisparity.sql
+++ dbt_snowflake/models/NBA_ShootingDisparity.sql
@@ -1,7 +1,21 @@
--- dbt_snowflake/models/example/food_trucks.sql
+-- dbt_snowflake/models/NBA_ShootingDIsparity.sql
 
-select TOP 10 * 
-from {{ source('NBA_SHOOTING', 'NBA_2425') }}
-where "3PFG%" is not null
-and MinutesPlayed > 500
-order by "3PFG%" desc
+SELECT
+    COALESCE(a.Player, b.Player) AS Player,
+    a."3PFG%" AS FG3_2324,
+    b."3PFG%" AS FG3_2425,
+    b."3PFG%" - a."3PFG%" AS FG3_Change
+FROM
+    {{ source('NBA_SHOOTING', 'NBA_2324') }} a
+FULL OUTER JOIN
+    {{ source('NBA_SHOOTING', 'NBA_2425') }} b
+    ON a.Player = b.Player
+WHERE 
+    a."3PFG%" IS NOT NULL
+    AND b."3PFG%" IS NOT NULL
+    and a.MinutesPlayed > 500
+    and b.MinutesPlayed > 500
+    and a."3PFG%" > 0
+    and b."3PFG%" > 0
+ORDER BY
+    FG3_Change DESC 
